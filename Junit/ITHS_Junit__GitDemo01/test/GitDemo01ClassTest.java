a46d1e2781ea6e8df6363600a398cd56e951a4c7
PerH_ITHS <per.hermansson@addq.se>
2019-02-06 09:40:09 +0100

    ITHS GitDemo01 in IntelliJ Third test added to test-class.

--- Junit/ITHS_Junit__GitDemo01/test/GitDemo01ClassTest.java
+++ Junit/ITHS_Junit__GitDemo01/test/GitDemo01ClassTest.java
@@ -30,4 +30,10 @@ public class GitDemo01ClassTest {
         assertTrue(result==0);
     }
 
+    @Test
+    public void test_alwayszero_normalCase3() {
+        GitDemo01Class myObject =new GitDemo01Class ();
+        int result= myObject.alwayszero(33, 33);
+        assertTrue(result==0);
+    }
 }
