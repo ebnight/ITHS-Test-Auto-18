1ac6f3a66fed2b81304836a330cd503e2b37eed7
Torgny Lyon <torgny@abc.se>
2019-02-07 09:39:37 +0100

    Flyttade instantieringen av myObject till setUp().

--- Junit/ITHS_Junit__GitDemo01/test/GitDemo01ClassTest.java
+++ Junit/ITHS_Junit__GitDemo01/test/GitDemo01ClassTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GitDemo01ClassTest {
+    private GitDemo01Class myObject;
 
     @Before
     public void setUp() throws Exception {
         System.out.println("Before - SetUp executed");
+        myObject = new GitDemo01Class();
     }
 
     @After
@@ -18,21 +20,18 @@ public class GitDemo01ClassTest {
 
     @Test
     public void test_alwayszero_normalCase() {
-        GitDemo01Class myObject =new GitDemo01Class ();
         int result= myObject.alwayszero(10, 5);
         assertTrue(result==0);
     }
 
     @Test
     public void test_alwayszero_normalCase2() {
-        GitDemo01Class myObject =new GitDemo01Class ();
         int result= myObject.alwayszero(99, 0);
         assertTrue(result==0);
     }
 
     @Test
     public void test_alwayszero_normalCase3() {
-        GitDemo01Class myObject =new GitDemo01Class ();
         int result= myObject.alwayszero(33, 33);
         assertTrue(result==0);
     }
