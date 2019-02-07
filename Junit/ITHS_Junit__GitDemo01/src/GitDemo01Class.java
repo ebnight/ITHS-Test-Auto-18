7c9d84db9660b77543bee23e8d6b16c944b5c302
Peter Englsperger <peter.englsperger@gmail.com>
2019-02-07 09:32:14 +0100

    added "add" method to GitDemo01Class and corresponding normal testcase to GitDemo01ClassTest

--- Junit/ITHS_Junit__GitDemo01/src/GitDemo01Class.java
+++ Junit/ITHS_Junit__GitDemo01/src/GitDemo01Class.java
@@ -1,9 +1,13 @@
 public class GitDemo01Class {
 
 
-    public int alwayszero(int a, int b)
-    {
+    public int alwayszero(int a, int b) {
         return 0;
     }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
 
 }
