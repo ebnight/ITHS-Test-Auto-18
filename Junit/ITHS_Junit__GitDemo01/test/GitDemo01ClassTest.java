de0adb00fdb65244dec8eee996bce54ecd528d22
ebnight <ebnight@users.noreply.github.com>
2019-02-07 10:05:33 +0100

    Shahzad

--- Junit/ITHS_Junit__GitDemo01/test/GitDemo01ClassTest.java
+++ Junit/ITHS_Junit__GitDemo01/test/GitDemo01ClassTest.java
@@ -44,4 +44,11 @@ public class GitDemo01ClassTest {
         int result= myObject.alwayszero(33, 33);
         assertTrue(result==0);
     }
+
+    @Test
+    public void shahzad2() {
+        GitDemo01Class myObject =new GitDemo01Class ();
+        int result= myObject.alwayszero(33, 33);
+        assertTrue(result==0);
+    }
 }
